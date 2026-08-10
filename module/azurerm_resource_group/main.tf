b75fb015a488b01fcce775f858e754119175d6db
deepaksingh017 <kumardeepaksing017@gmail.com>
2026-08-10 23:27:09 +0530

    fmt error fixed RG group

--- module/azurerm_resource_group/main.tf
+++ module/azurerm_resource_group/main.tf
@@ -1,5 +1,5 @@
 resource "azurerm_resource_group" "frondend_prod" {
-    for_each = var.resource_group
-    name = each.value.name
-    location = each.value.location
+  for_each = var.resource_group
+  name     = each.value.name
+  location = each.value.location
 }
