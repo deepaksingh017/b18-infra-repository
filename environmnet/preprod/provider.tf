de6b30b4dc7d7038803615b7fdcd78d663f27ffc
deepaksingh017 <kumardeepaksing017@gmail.com>
2026-08-13 23:13:28 +0530

    update provider class

--- environmnet/preprod/provider.tf
+++ environmnet/preprod/provider.tf
@@ -5,10 +5,13 @@ terraform {
       version = "4.80.0"
     }
   }
+  # backend "azurerm" {
+  #   resource_group_name  = "rg1"
+  #   storage_account_name = "str1"
+  #   container_name       = "tfstate"
+  #   key                  = "terraform.tfstate"
+  # }
 }
-
 provider "azurerm" {
-  features {
-
-  }
+  features {}
 }
